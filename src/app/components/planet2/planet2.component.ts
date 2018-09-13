39777f882d362b864ce3bf501877363a2026f75a
Rohen Steenkamp <rohensteenkamp@gmail.com>
2018-09-13 13:50:42 +0200

    Read of planets from observable, shows on list

--- src/app/components/planet2/planet2.component.ts
+++ src/app/components/planet2/planet2.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Planet2Service } from '../../services/planet2.service';
+import { Planet } from '../../models/planet';
 
 @Component({
   selector: 'app-planet2',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planet2.component.css']
 })
 export class Planet2Component implements OnInit {
+  planets: Planet[];
+  newPlanet: Planet = {} as Planet;
 
-  constructor() { }
+  constructor(private planet2Service: Planet2Service) { }
 
   ngOnInit() {
+    this.planet2Service.planets$.subscribe(value => this.planets = value)
   }
 
+  addPlanet(): void {
+    this.planet2Service.addPlanet(this.newPlanet);
+    this.newPlanet = {} as Planet;
+  }
 }
--- src/app/services/planet2.service.ts
+++ src/app/services/planet2.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Planet } from '../models/planet';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,9 +8,19 @@ import { Observable } from 'rxjs';
 })
 export class Planet2Service {
   planets$: Observable<any[]>;
-  newPlanet: Planet = {} as Planet;
+  planetCollection: AngularFirestoreCollection<Planet>;
 
   constructor(db: AngularFirestore) {
+    this.planetCollection = db.collection<Planet>("planets");
     this.planets$ = db.collection("planets").valueChanges();
    }
+   
+  getPlanets() {
+    return null;
+  }
+
+  addPlanet(planet: Planet) {
+    this.planetCollection.add(planet);
+  }
+
 }
